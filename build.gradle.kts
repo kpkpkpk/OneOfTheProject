7db9015e30c515499d8dfd0cb9511034b8ceec8b
ko.ponomarev <ko.ponomarev@tinkoff.ru>
2022-10-06 22:11:03 +0300

    setup detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,25 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.detekt)
     id("com.android.application") version "7.2.1" apply false
     id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("io.gitlab.arturbosch.detekt") version libs.versions.detekt
 }
 allprojects {
 
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
     dependencies {
-        detektPlugins("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0")
-        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
+        /**using rootProject.project instead of
+         * simple libs because gradle don't fix problem with access
+         * see: https://github.com/gradle/gradle/issues/16634#issuecomment-809345790
+         **/
+        detektPlugins(rootProject.project.libs.util.detektPlugin)
+        detektPlugins(rootProject.project.libs.util.detektFormatting)
     }
     tasks.withType<Detekt>().configureEach {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
         setSource(files("src"))
         exclude("**/androidTest/**")
         exclude("**/assets/**")
@@ -31,11 +35,11 @@ allprojects {
         }
     }
     tasks.withType<DetektCreateBaselineTask>().configureEach {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     configure<DetektExtension> {
-        toolVersion = "1.21.0"
+        toolVersion = rootProject.project.libs.versions.detekt.get()
         buildUponDefaultConfig = true
         autoCorrect = true
     }
