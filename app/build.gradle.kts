64329c07462aef7d8082b1c2f5a34641c1b561e8
Konstantin <66991334+kpkpkpk@users.noreply.github.com>
2022-10-23 21:44:56 +0300

    Докинул зависимостей в проект (#10)
    подтянул зависимости в проект

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
+    id("kotlin-kapt")
     kotlin("android")
 }
 
@@ -36,8 +39,8 @@ android {
         kotlinCompilerExtensionVersion = "1.3.1"
     }
     buildFeatures {
-        // Enables Jetpack Compose for this module
         compose = true
+        viewBinding = true
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -48,6 +51,13 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    implementation(libs.dagger.core)
+    kapt(libs.dagger.compiler)
+    testImplementation(libs.bundles.testsBundle)
+    implementation(libs.terrakok.cicerone)
+    implementation(libs.bundles.coil)
+    implementation(libs.bundles.coroutines)
     implementation(libs.bundles.androidx)
-
+    implementation(libs.bundles.okhttpRetrofitBundle)
+    implementation(libs.viewBindingPropertyDelegate)
 }
--- app/src/main/java/com/kponomarev/oneofthenewproject/MainActivity.kt
+++ app/src/main/java/com/kponomarev/oneofthenewproject/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kponomarev.oneofthenewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.kponomarev.oneofthenewproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+@Suppress("UnusedPrivateMember")
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding: ActivityMainBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
