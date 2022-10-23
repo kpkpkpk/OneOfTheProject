4157c2d238ee8c68b4e9703db5078687bfd4d359
ko.ponomarev <ko.ponomarev@tinkoff.ru>
2022-10-23 21:44:06 +0300

    добавил зависимости в проект

--- app/src/main/java/com/kponomarev/oneofthenewproject/MainActivity.kt
+++ app/src/main/java/com/kponomarev/oneofthenewproject/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kponomarev.oneofthenewproject.databinding.ActivityMainBinding
 
+@Suppress("UnusedPrivateMember")
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
     private val binding: ActivityMainBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
